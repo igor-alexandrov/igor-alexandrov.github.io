9b107b90afa567bb23f44723746b63239c3de52f
Igor Alexandrov <igor.alexandrov@gmail.com>
2024-02-04 12:39:18 +0400

    Add OG image data to all posts

--- _plugins/og_image_generator.rb
+++ _plugins/og_image_generator.rb
@@ -8,45 +8,57 @@ module SamplePlugin
       FileUtils.mkdir_p "assets/og-images/posts"
 
       site.posts.docs.each do |post|
-        filename = "assets/og-images/posts/#{post.slug}.png"
+        filename = "assets/og-images/posts/#{post.data['slug']}.png"
+        generate_image_for_post(site, post, filename)
 
-        date = post.date.strftime("%B %d, %Y")
+        post.data['image'] ||= {
+          'path' => filename,
+          'width' => 1200,
+          'height' => 600,
+          'alt' => post.data['title']
+        }
+      end
+    end
 
-        image = JekyllOpengraph::Image.new(1200, 600)
-          .border(20, position: :bottom, fill: [ '#820C02', '#A91401', '#D51F06', '#DE3F24', '#EDA895', '#FFFFFF' ])
-          .text(post.title, width: 1040, color: "#2f313d", dpi: 500, font: 'Helvetica, Bold') do |_canvas, _text|
-            {
-              x: 80,
-              y: 100
-            }
-          end
-          .text(date, gravity: :sw, color: "#535358", dpi: 200, font: 'Helvetica, Regular') do |_canvas, _text|
-            {
-              x: 80,
-              y: post.tags.any? ? 150 : 100
-            }
-          end
+    private
 
-        if post.tags.any?
-          tags = post.tags.map { |tag| "##{tag}" }.join(" ")
+    def generate_image_for_post(site, post, path)
+      date = post.date.strftime("%B %d, %Y")
 
-          image = image.text(tags, gravity: :sw, color: "#535358", dpi: 150, font: 'Helvetica, Regular') do |_canvas, _text|
-            {
-              x: 80,
-              y: 100
-            }
-          end
-        end
-
-        image = image.text(site.baseurl || 'http://127.0.0.1:4000', gravity: :se, color: "#535358", dpi: 200, font: 'Helvetica, Regular') do |_canvas, _text|
+      image = JekyllOpengraph::Image.new(1200, 600)
+        .border(20, position: :bottom, fill: [ '#820C02', '#A91401', '#D51F06', '#DE3F24', '#EDA895', '#FFFFFF' ])
+        .text(post.data['title'], width: 1040, color: "#2f313d", dpi: 500, font: 'Helvetica, Bold') do |_canvas, _text|
           {
             x: 80,
-            y: post.tags.any? ? 150 : 100
+            y: 100
+          }
+        end
+        .text(date, gravity: :sw, color: "#535358", dpi: 200, font: 'Helvetica, Regular') do |_canvas, _text|
+          {
+            x: 80,
+            y: post.data['tags'].any? ? 150 : 100
           }
         end
 
-        image.save(filename)
+      if post.data['tags'].any?
+        tags = post.data['tags'].map { |tag| "##{tag}" }.join(" ")
+
+        image = image.text(tags, gravity: :sw, color: "#535358", dpi: 150, font: 'Helvetica, Regular') do |_canvas, _text|
+          {
+            x: 80,
+            y: 100
+          }
+        end
       end
+
+      image = image.text('igor.works', gravity: :se, color: "#535358", dpi: 200, font: 'Helvetica, Regular') do |_canvas, _text|
+        {
+          x: 80,
+          y: post.tags.any? ? 150 : 100
+        }
+      end
+
+      image.save(path)
     end
   end
 end
