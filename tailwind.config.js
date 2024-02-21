d1a65b94239a46dfeec93512d6ebea24a57fb285
Igor Alexandrov <igor.alexandrov@gmail.com>
2024-02-21 11:45:24 +0400

    Added initial Tailwind layout

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./_layouts/**/default.html",
+    "./_layouts/**/page.html",
+    "./_layouts/**/post.html",
+    "./_includes/**/*.html",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        lato: ['"Lato"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
