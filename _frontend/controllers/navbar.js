c4f48d3db0ccd7e050c6cfbc26f3722da160c74f
Igor Alexandrov <igor.alexandrov@gmail.com>
2024-02-21 11:45:24 +0400

    Added initial Tailwind layout

---
+++ _frontend/controllers/navbar.js
@@ -0,0 +1,41 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class NavbarController extends Controller {
+  static targets = ['menu'];
+
+  static values = { open: Boolean };
+
+  connect() {
+    this.toggleClass = this.data.get('class') || 'hidden';
+  }
+
+  disconnect() {
+    this.hide();
+  }
+
+  toggle() {
+    this.openValue = !this.openValue;
+  }
+
+  openValueChanged() {
+    if (this.openValue) {
+      this.show();
+    } else {
+      this.hide();
+    }
+  }
+
+  show() {
+    this.element.classList.add('overflow-hidden', 'md:overflow-auto');
+    if(this.hasMenuTarget) {
+      this.menuTarget.classList.remove(this.toggleClass);
+    }
+  }
+
+  hide() {
+    this.element.classList.remove('overflow-hidden', 'md:overflow-auto');
+    if(this.hasMenuTarget) {
+      this.menuTarget.classList.add(this.toggleClass);
+    }
+  }
+}
