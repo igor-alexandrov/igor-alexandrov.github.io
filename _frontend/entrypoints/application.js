fb477c367eed9451a0c9a3428972c4bda9e287e2
Igor Alexandrov <igor.alexandrov@gmail.com>
2024-05-07 20:47:27 +0400

    Tailwind Markup (#1)
    * Switched to the Tailwind CSS framework

--- _frontend/entrypoints/application.js
+++ _frontend/entrypoints/application.js
@@ -6,15 +6,33 @@
 // Example: Import a stylesheet in <sourceCodeDir>/index.css
 // import '~/index.css'
 
-import '~/prism';
+import "~/prism";
 
-import { Application } from '@hotwired/stimulus'
-import ScrollTo from 'stimulus-scroll-to'
-import Reveal from 'stimulus-reveal-controller'
-import ScrollProgress from 'stimulus-scroll-progress'
+import { Application } from "@hotwired/stimulus";
+import ScrollTo from "stimulus-scroll-to";
+import SlideOver from "../controllers/slide-over-controller";
+import ScrollProgress from "stimulus-scroll-progress";
+import TextareaAutogrow from 'stimulus-textarea-autogrow'
 
-const application = Application.start()
+import { createIcons, Menu, Twitter, Github, Youtube, Linkedin, ChevronLeft, ChevronRight, CircleArrowRight, X } from 'lucide';
 
-application.register('scroll-to', ScrollTo)
-application.register('reveal', Reveal)
-application.register('scroll-progress', ScrollProgress)
+const application = Application.start();
+
+application.register("scroll-to", ScrollTo);
+application.register("slide-over", SlideOver);
+application.register("scroll-progress", ScrollProgress);
+application.register('textarea-autogrow', TextareaAutogrow);
+
+createIcons({
+  icons: {
+    Menu,
+    Twitter,
+    Github,
+    Youtube,
+    Linkedin,
+    ChevronLeft,
+    ChevronRight,
+    CircleArrowRight,
+    X
+  }
+});
---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./_layouts/**/default.html",
+    "./_layouts/**/page.html",
+    "./_layouts/**/post.html",
+    "./_includes/**/*.html",
+    "*.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        lato: ['"Lato"', "sans-serif"],
+        "ubuntu-mono": ['"Ubuntu Mono"', "monospace"],
+      },
+      colors: {
+        'ruby': {
+          '50': '#fff2f0',
+          '100': '#ffe2de',
+          '200': '#ffcac3',
+          '300': '#ffa599',
+          '400': '#ff715e',
+          '500': '#ff452c',
+          '600': '#f6280c',
+          '700': '#d51f06',
+          '800': '#ab1d09',
+          '900': '#8d1e0f',
+          '950': '#4d0b02',
+        },
+      },
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            a: {
+              color: theme("colors.ruby.700"),
+              "&:hover": {
+                color: theme("colors.ruby.500"),
+              },
+            },
+          },
+        },
+      }),
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
